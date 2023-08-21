fa8f10d424628475f3597dca235c4fd56ecbea78
Chris92i <chris.p.teixeira@gmail.com>
2023-08-21 18:15:16 +0100

    ajout de commentaires pour la compréhension

--- back/Routes/user.js
+++ back/Routes/user.js
@@ -1,7 +1,13 @@
+// import d'express
 const express = require('express')
+
+// création du Router
 const router = express.Router()
+
 const userctrl = require("../controllers/user")
 const validpassword = require('../middleware/valid')
+
+
 router.post("/signup",validpassword,userctrl.signup)
 
 router.post("/login",userctrl.login)
@@ -9,5 +15,5 @@ router.post("/login",userctrl.login)
 
 
 
-
+// exporter le Routeur
 module.exports = router
