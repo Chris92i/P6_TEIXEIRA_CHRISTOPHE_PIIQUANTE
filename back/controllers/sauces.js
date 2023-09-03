36cf1b52791233fe2b6b98d4dcaf0997ea658b75
Chris92i <chris.p.teixeira@gmail.com>
2023-09-03 17:07:35 +0200

    modification de updatesauce du controllers sauce

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -53,55 +53,36 @@ exports.getOneSauce = (req, res) => {
 };
 
 exports.updateSauce = (req, res) => {
-  const sauceObject = req.file?{
-    ...JSON.parse(req.body.sauce),
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`}
-    :{
-      ...req.body
-    }
-    Sauce.findOne({_id: req.params.id})
-    .then((sauce) => {
-      // if(sauce.userId != req.auth.userId){
-      //   res.status(401).json({message: "non autorisé"})
-      // }
-      //else{
-        Sauce.updateOne({ _id: req.params.id }, {...sauceObject,_id:req.params.id})
-        .then(() => {
-        res.status(201).json({
-        message: "Sauce updated successfully!"})
-        .catch(error => res.status(500).json({error})) 
+  const sauceObject = req.file
+      ? {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${
+                req.file.filename
+            }`,
+        }
+      : {
+            ...req.body,
+        };
+  Sauce.findOne({ _id: req.params.id })
+      .then((sauce) => {
+          if (sauce.userId != req.auth.userId) {
+              res.status(401).json({ message: "non autorisé" });
+          } else {                
+              Sauce.updateOne(
+                  { _id: req.params.id },
+                  { ...sauceObject, _id: req.params.id }
+              )
+                  .then(() => {
+                      res.status(201).json({
+                          message: "Sauce updated successfully!",
+                      });
+                  })
+                  .catch((error) => res.status(500).json({ error }));
+          }
+      })
+      .catch((error) => {
+          res.status(404).json({ error });
       });
-      //}
-    })
-    .catch((error) => {
-      res.status(404).json({
-        error: error,
-      });
-    });
-
-
-
-
-
-  const sauce = new Sauce({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId,
-  });
-  Sauce.updateOne({ _id: req.params.id }, sauce)
-    .then(() => {
-      res.status(201).json({
-        message: "Sauce updated successfully!",
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
 };
 
 exports.deleteSauce = (req, res) => {
