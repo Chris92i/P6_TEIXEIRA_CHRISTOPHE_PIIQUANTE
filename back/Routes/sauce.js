31c498a69a052f1bc4adff380b3dd445ce57a415
Chris92i <chris.p.teixeira@gmail.com>
2023-09-03 23:02:29 +0200

    ajout du middleware multer sur la route PUT

--- back/Routes/sauce.js
+++ back/Routes/sauce.js
@@ -11,7 +11,7 @@ router.get("/:id", auth, sauceCtrl.getOneSauce);
 
 router.post("/", auth, multer, sauceCtrl.createSauce);
 
-router.put("/:id", auth, sauceCtrl.updateSauce);
+router.put("/:id", auth, multer, sauceCtrl.updateSauce);
 
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
 
